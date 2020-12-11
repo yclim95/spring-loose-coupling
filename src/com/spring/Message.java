dd5711d9cae2639bd83e71963579d72dd73f54b2
yclim95 <lyc950421@gmail.com>
2020-12-11 14:34:30 +0800

    Initial commit

---
+++ src/com/spring/Message.java
@@ -0,0 +1,20 @@
+package com.spring;
+
+public class Message {
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "Message: " + message;
+	}
+	
+	
+}
---
+++ src/com/spring/Test.java
@@ -0,0 +1,14 @@
+package com.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		Message message = (Message) context.getBean("test");
+		System.out.println(message);
+	}
+
+}
